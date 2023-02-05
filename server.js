403290fde8b4f60ab057615449d7a40cd5bc563a
Danielle Cavinder <daniellecavinder@gmail.com>
2023-02-05 11:27:02 -0700

    installed node modules, added server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.join(_dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({ extend: true }));
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(process.env.port || 3000);
+
